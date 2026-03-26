2e1a792ab5a7d85899a314d5fbe529e42103627f
Trusty <ivanbordeira2015@hotmail.com>
2026-03-26 17:59:48 -0300

    Modificaciones de whaler

--- tp1.X/ejercicio1.c
+++ tp1.X/ejercicio1.c
@@ -6,31 +6,37 @@
  */
 #include "xc.h"
 #include "ejercicio1.h"
+#include <p33FJ256GP710.h>
 // ----------------->>>EJERCICIO 1 <<<------------------------
 //Pulsadores y controles.
 typedef union {
     struct{
-        unsigned :6;
-        unsigned PS6ON:1;
-        unsigned PS7OFF:1;
-        unsigned :5;
-        unsigned PS13SEG:1;
-        unsigned :2;
+        unsigned int LED0VERDE: 1;
+        unsigned int LED1ROJO: 1;
+        unsigned int : 14;
+    }bits;
+    unsigned int Leds;
+} leds_t;
+
+typedef union {
+    struct{
+        unsigned int :6;
+        unsigned int PS6ON:1;
+        unsigned int PS7OFF:1;
+        unsigned int :5;
+        unsigned int PS13SEG:1;
+        unsigned int :2;
     };
     unsigned Controles;
 } controles_t;
 
 //Leds
-typedef union {
-    struct{
-        unsigned LED0VERDE: 1;
-        unsigned LED1ROJO: 1;
-        unsigned : 14;
-    };
-    unsigned Leds;
-} leds_t;
+    leds_t leds;
+    controles_t controles;
 
 void Ejercicio1(){
+    //Inicializo structs
+
     //configuracion de pines
     // TRISA leds
     TRISA = 0x0000;
@@ -44,25 +50,22 @@ void Ejercicio1(){
     //El led verde deberia estar en 0 por default, pero me aseguro
     LATAbits.LATA0 = 0;
     
-    //Inicializo structs
-    controles_t controles;
-    leds_t leds;
     
     while(1){
         controles.Controles = PORTD;
-        leds.Leds = PORTA;
+        leds.bits.LED0VERDE = 1;
         if(controles.PS13SEG == 0 && controles.PS7OFF == 0){
-            leds.LED0VERDE = 1;
+            leds.bits.LED0VERDE = 1;
             //Realiza el trabajo trabajoso de manera muy trabajadora
         }    
         if(controles.PS13SEG == 1){
-            leds.LED0VERDE = 0;
-            leds.LED1ROJO = 1;
+            leds.bits.LED0VERDE = 0;
+            leds.bits.LED1ROJO = 1;
             //Aviso de que no se cumple con la condición de seguridad.
         }
         if(controles.PS7OFF){
-            leds.LED0VERDE = 0;
-            leds.LED1ROJO = 1;
+            leds.bits.LED0VERDE = 0;
+            leds.bits.LED1ROJO = 1;
             //Detención subita del programa
             return 0;
         }
