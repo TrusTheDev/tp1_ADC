2ce988ed868d7d0dd83b4cd120f799f04886b102
Trusty <ivanbordeira2015@hotmail.com>
2026-03-27 23:05:49 -0300

    Ejercicio 1 resuelto

--- tp1.X/ejercicio1.c
+++ tp1.X/ejercicio1.c
@@ -9,12 +9,18 @@
 #include <p33FJ256GP710.h>
 // ----------------->>>EJERCICIO 1 <<<------------------------
 //Pulsadores y controles.
+
+/* Stimulus:
+ * RD6 = Pulsador de encendido
+ * RD7 = Pulsador de apagado
+ * RD13 = condicion de seguridad
+ */
 typedef union {
     struct{
         unsigned int LED0VERDE: 1;
         unsigned int LED1ROJO: 1;
         unsigned int : 14;
-    }bits;
+    };
     unsigned int Leds;
 } leds_t;
 
@@ -40,33 +46,36 @@ void Ejercicio1(){
     //configuracion de pines
     // TRISA leds
     TRISA = 0x0000;
-    // TRISD controles
+    // TRISD controles como entrada
     TRISD = 0X20C0;
-    
-    TRISD = 0x0;
-    
     //La maquina empieza detenida (led rojo)
     LATAbits.LATA1 = 1;
     //El led verde deberia estar en 0 por default, pero me aseguro
     LATAbits.LATA0 = 0;
     
-    
     while(1){
         controles.Controles = PORTD;
-        leds.bits.LED0VERDE = 1;
-        if(controles.PS13SEG == 0 && controles.PS7OFF == 0){
-            leds.bits.LED0VERDE = 1;
+        leds.Leds = PORTA;
+        if(controles.PS13SEG && !controles.PS7OFF){
+            //Apago el led rojo
+            LATAbits.LATA1 = 0;
+            //Prendo el led verde
+            LATAbits.LATA0 = 1;
             //Realiza el trabajo trabajoso de manera muy trabajadora
         }    
-        if(controles.PS13SEG == 1){
-            leds.bits.LED0VERDE = 0;
-            leds.bits.LED1ROJO = 1;
-            //Aviso de que no se cumple con la condici�n de seguridad.
+        // Si la maquina esta prendida pero la condicion de seguridad no se cumple 
+        //y no esta activada la detencion
+        if(controles.PS6ON && !controles.PS13SEG && !controles.PS7OFF){
+            //Apago el led verde
+            LATAbits.LATA0 = 0;
+            //Prendo el led rojo
+            LATAbits.LATA1 = 1;
+            //Aviso de que no se cumple con la condicion de seguridad.
         }
         if(controles.PS7OFF){
-            leds.bits.LED0VERDE = 0;
-            leds.bits.LED1ROJO = 1;
-            //Detenci�n subita del programa
+            LATAbits.LATA0 = 0;
+            LATAbits.LATA1 = 1;
+            //Detencion subita del programa 
             return 0;
         }
     }   
